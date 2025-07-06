cf3d08e276a0e39d3dfd3ee53aab5d46b891a198
nebuff <bitfruit8@gmail.com>
2025-07-06 18:29:54 -0400

    Update drivemanager.sh

--- apps/drivemanager.sh
+++ apps/drivemanager.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-# Directory to use for mounting drives (make sure user can access)
 MOUNT_BASE="$HOME/mnt"
 mkdir -p "$MOUNT_BASE"
 
 while true; do
-  # Get all block devices with partitions
   drives=()
-  # List partitions (exclude loop, ram, and removable by default)
   mapfile -t drives < <(lsblk -lnpo NAME,SIZE,TYPE,MOUNTPOINT | grep 'part' | awk '{print $1}')
 
-  # Prepare dialog menu items
   menu_items=()
   for dev in "${drives[@]}"; do
     size=$(lsblk -ndo SIZE "$dev")
@@ -32,44 +28,38 @@ while true; do
   ret=$?
   [ $ret -ne 0 ] && break
 
-  # Submenu for actions on selected drive
   while true; do
     mpath=$(lsblk -ndo MOUNTPOINT "$choice")
     size=$(lsblk -ndo SIZE "$choice")
 
-    if [ -z "$mpath" ]; then
-      m_status="Unmounted"
-      options=(1 "Mount drive" 2 "Back")
-    else
-      m_status="Mounted at $mpath"
-      options=(1 "Unmount drive" 2 "Browse drive" 3 "Back")
+    options=(1 "Mount / Unmount")
+    if [ -n "$mpath" ]; then
+      options+=(2 "Browse")
     fi
+    options+=(3 "Format Drive" 4 "Other Options" 5 "Back")
 
     action=$(dialog --backtitle "T41 Drive Manager" --title "Drive: $choice ($size)" \
-      --menu "Status: $m_status\nSelect action:" 15 50 4 "${options[@]}" 3>&1 1>&2 2>&3)
+      --menu "Status: ${mpath:-Unmounted}\nSelect action:" 18 60 10 "${options[@]}" 3>&1 1>&2 2>&3)
 
     case $action in
       1)
         if [ -z "$mpath" ]; then
-          # Mount the drive - ask for mount point
           default_mp="$MOUNT_BASE/$(basename $choice)"
-          mount_point=$(dialog --inputbox "Enter mount point directory (will be created if needed):" 8 60 "$default_mp" 3>&1 1>&2 2>&3)
-          if [ -n "$mount_point" ]; then
-            mkdir -p "$mount_point"
-            sudo mount "$choice" "$mount_point" 2>/tmp/mount_error
-            if [ $? -eq 0 ]; then
-              dialog --msgbox "Mounted $choice at $mount_point" 6 50
-              break
-            else
-              err=$(cat /tmp/mount_error)
-              dialog --msgbox "Failed to mount:\n$err" 8 60
-            fi
+          mount_point=$(dialog --inputbox "Enter mount point directory:" 8 60 "$default_mp" 3>&1 1>&2 2>&3)
+          [ -z "$mount_point" ] && continue
+          mkdir -p "$mount_point"
+          sudo mount "$choice" "$mount_point" 2>/tmp/mount_error
+          if [ $? -eq 0 ]; then
+            dialog --msgbox "Mounted $choice at $mount_point" 6 50
+            break
+          else
+            err=$(cat /tmp/mount_error)
+            dialog --msgbox "Failed to mount:\n$err" 8 60
           fi
         else
-          # Unmount drive
           sudo umount "$choice" 2>/tmp/umount_error
           if [ $? -eq 0 ]; then
-            dialog --msgbox "Unmounted $choice from $mpath" 6 50
+            dialog --msgbox "Unmounted $choice" 6 40
             break
           else
             err=$(cat /tmp/umount_error)
@@ -78,48 +68,51 @@ while true; do
         fi
         ;;
       2)
-        if [ -z "$mpath" ]; then
-          break
-        else
-          # Browse mounted directory
-          while true; do
-            entries=()
-            mapfile -t files < <(ls -A "$mpath")
-            [ ${#files[@]} -eq 0 ] && dialog --msgbox "Directory is empty." 6 40 && break
-
-            # Build list with indices to handle dialog selection
-            for i in "${!files[@]}"; do
-              f="${files[$i]}"
-              if [ -d "$mpath/$f" ]; then
-                entries+=("$i" "[DIR] $f")
-              else
-                entries+=("$i" "$f")
-              fi
-            done
-
-            sel=$(dialog --menu "Browsing: $mpath\nSelect file or directory:" 20 70 15 "${entries[@]}" 3>&1 1>&2 2>&3)
-            [ $? -ne 0 ] && break
-
-            selfile="${files[$sel]}"
-            fullpath="$mpath/$selfile"
-
-            if [ -d "$fullpath" ]; then
-              # Enter directory
-              mpath="$fullpath"
+        [ -z "$mpath" ] && continue
+        while true; do
+          entries=()
+          mapfile -t files < <(ls -A "$mpath")
+          [ ${#files[@]} -eq 0 ] && dialog --msgbox "Directory is empty." 6 40 && break
+          for i in "${!files[@]}"; do
+            f="${files[$i]}"
+            if [ -d "$mpath/$f" ]; then
+              entries+=("$i" "[DIR] $f")
             else
-              # Show file content (first 100 lines)
-              dialog --textbox "$fullpath" 20 70
+              entries+=("$i" "$f")
             fi
           done
-        fi
+          sel=$(dialog --menu "Browsing: $mpath\nSelect:" 20 70 15 "${entries[@]}" 3>&1 1>&2 2>&3)
+          [ $? -ne 0 ] && break
+          selfile="${files[$sel]}"
+          fullpath="$mpath/$selfile"
+          if [ -d "$fullpath" ]; then
+            mpath="$fullpath"
+          else
+            dialog --textbox "$fullpath" 20 70
+          fi
+        done
         ;;
       3)
-        # Back
-        break
+        confirm=$(dialog --yesno "Are you sure you want to format $choice?\nAll data will be lost!" 8 60)
+        if [ $? -eq 0 ]; then
+          fstype=$(dialog --inputbox "Enter filesystem type (e.g. ext4, vfat):" 8 40 "ext4" 3>&1 1>&2 2>&3)
+          [ -z "$fstype" ] && continue
+          sudo mkfs."$fstype" "$choice" 2>/tmp/format_err
+          if [ $? -eq 0 ]; then
+            dialog --msgbox "$choice formatted as $fstype." 6 50
+          else
+            err=$(cat /tmp/format_err)
+            dialog --msgbox "Format failed:\n$err" 8 60
+          fi
+        fi
         ;;
-      *)
-        break
+      4)
+        info=$(lsblk -f "$choice"; echo ""; sudo blkid "$choice")
+        echo "$info" > /tmp/drive_info.txt
+        dialog --textbox /tmp/drive_info.txt 20 60
         ;;
+      5) break ;;
+      *) break ;;
     esac
   done
 done
